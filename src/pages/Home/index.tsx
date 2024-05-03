7a2bf3d28b738a29f161d5c29031504237005150
lcht1 <manuleichtveis@gmail.com>
2024-05-03 17:05:33 -0300

    feat: add message home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { ButtonsContainer, Container } from './styles'
+import { ButtonsContainer, Container, Info, Main } from './styles'
 
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
@@ -9,14 +9,20 @@ export const Home = () => {
     <Container>
       <Header />
 
-      <main>
+      <Main>
         <ButtonsContainer>
           <Link to="preciso-ajuda">
             <Button color="red">PRECISO DE AJUDA</Button>
           </Link>
           <Button color="yellow">QUERO AJUDAR</Button>
         </ButtonsContainer>
-      </main>
+        <Info>
+          Por favor, esteja ciente de que este aplicativo não é uma iniciativa oficial do governo. Foi criado com o
+          propósito de unir a comunidade em tempos de necessidade, para que possamos nos ajudar mutuamente durante as
+          enchentes.
+        </Info>
+      </Main>
     </Container>
   )
 }
+
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,15 +1,21 @@
 import styled from 'styled-components'
+import { theme } from '../../styles/Theme'
 
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
-
   main {
     padding: 5rem;
     align-self: center;
   }
+`
+export const Main = styled.main`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `
 
 export const ButtonsContainer = styled.div`
@@ -18,3 +24,12 @@ export const ButtonsContainer = styled.div`
   flex-direction: column;
   gap: 15px;
 `
+export const Info = styled.p`
+  text-align: justify;
+  font-style: italic;
+  font-size: ${theme.fontSize.sm};
+  color: ${theme.colors.black50Light};
+
+  line-height: 1.3;
+`
+
