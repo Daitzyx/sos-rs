bba85c5b45792d60a42d4e2d6c007fcaf7d724f4
Daitzyx <nathanrtd01@gmail.com>
2024-05-28 02:19:44 -0300

    refact: Removing comments

--- src/utils/formatCep.tsx
+++ src/utils/formatCep.tsx
@@ -1,13 +1,9 @@
 export const formatCep = (value: any) => {
-  // Remove caracteres não numéricos
   const numericValue = value.replace(/\D/g, '')
 
-  // Verifica se o valor tem mais de 5 dígitos
   if (numericValue.length > 5) {
-    // Formata como "XXXXX-XXX"
     return `${numericValue.slice(0, 5)}-${numericValue.slice(5, 8)}`
   } else {
-    // Retorna o valor original
     return numericValue
   }
 }
